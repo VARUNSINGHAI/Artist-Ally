b6c8a608affb0ad04edd4e32b91a505acfaa1af3
varun singhai <varun01singhai01@gmail.com>
2020-11-24 21:34:24 +0530

    Done till letsidebar

---
+++ src/Header.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import HomeIcon from '@material-ui/icons/Home';
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import { Avatar , IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import CreateIcon from '@material-ui/icons/Create';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://urbanjungle.ca/wp-content/uploads/2014/01/letter-a-logo-uppercase.jpg" alt=""/>
+                <div className="header__search">
+                    <SearchIcon />
+                    <input placeholder="Search Artist Ally..." type="text"/>
+                </div>
+                
+            </div>
+
+            <div className="header__middle">
+                <div className="header__options
+                header__options--active">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__options">
+                    <AccountTreeIcon fontSize="large" />
+                </div>
+                <div className="header__options">
+                    <EventAvailableIcon fontSize="large" />
+                </div>
+            </div>
+
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Varun</h4>
+                </div>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+                <IconButton>
+                    <CreateIcon />
+                </IconButton>
+        
+            </div>
+                   
+
+        </div>
+    );
+    
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBcNAqXWyuucQ27FMe6ptutWy3AfR3ipLU",
+    authDomain: "artist-ally-eb047.firebaseapp.com",
+    databaseURL: "https://artist-ally-eb047.firebaseio.com",
+    projectId: "artist-ally-eb047",
+    storageBucket: "artist-ally-eb047.appspot.com",
+    messagingSenderId: "472530904119",
+    appId: "1:472530904119:web:606d254b6e6c62c5c4cbb2",
+    measurementId: "G-GDF0BG79CM"
+  };
