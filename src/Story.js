da7a8d0ac5811ef261904f36724981a1e011e4af
varun singhai <varun01singhai01@gmail.com>
2020-11-24 23:34:34 +0530

    Merge branch 'StoryReels1' of https://github.com/VARUNSINGHAI/Artist-Ally into StoryReels1

---
+++ src/Story.js
@@ -0,0 +1,16 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import "./Story.css";
+{/*so basically the story reel have three things the image the profile pic and the name */}
+{/*Image-image,Profile Pic-profileSrc and Name as title*/}
+function Story({image,profileSrc,title}) {
+    return (
+        <div className="story">
+            <Avatar src={profileSrc}/>
+            <h4>{title}</h4>
+            
+        </div>
+    );
+}
+
+export default Story;
