8fa6bf48d2078b8d701f4e8171cb10481d7c2b44
varun singhai <varun01singhai01@gmail.com>
2020-11-24 23:34:34 +0530

    Update-1 on StoryReels

---
+++ src/StoryReel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Story from './Story';
+import "./StoryReel.css";
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story
+              image=""
+              profileSrc=""
+              title="Varun Singhai"
+
+            />
+            <Story
+              image=""
+              profileSrc=""
+              title="Shubham Singh"
+
+            />
+            
+        </div>
+    );
+}
+
+export default StoryReel;
